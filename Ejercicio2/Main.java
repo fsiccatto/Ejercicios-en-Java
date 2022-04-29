ed722bae5f42200c0db1a6e4b643587f015aac7c
Franco Siccatto <fsiccatto@gmail.com>
2022-04-29 10:21:06 -0300

    Ejercicio 2 en Java

---
+++ Ejercicio2/Main.java
@@ -0,0 +1,37 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        // Con este codigo pedimos un número
+        System.out.println("Ingrese el primer número:");
+        Integer numero1 = cargarNumero();
+        
+        System.out.println("Ingrese el segundo número:");
+        Integer numero2 = cargarNumero();
+        
+        System.out.println("Ingrese el tercer número:");
+        Integer numero3 = cargarNumero();
+        
+        Integer sumatoria = numero1 + numero2 + numero3;
+        Double numeroPromedio = Double.valueOf(sumatoria) / 3;
+        System.out.println("El número promedio es: " + numeroPromedio);
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Integer cargarNumero() throws IOException {
+        InputStreamReader capturarTeclado = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(capturarTeclado);
+        String strNumero = buffer.readLine();
+        Integer numero = Integer.parseInt(strNumero);
+        return numero;
+    }
+    
+}
